17b41ebf5058725f004425528d5fdf8bcbc47c0b
mjoel17 <mandezgonzalez.l@northeastern.edu>
2021-10-05 20:45:42 -0400

    Fatal

---
+++ homework/10-5/numbers.js
@@ -0,0 +1,19 @@
+<script>
+
+            var myNumber = 45;
+
+            console.log(myNumber);
+
+            myNumber = myNumber + 20;
+
+            console.log(myNumber);
+
+            myNumber = myNumber * 10;
+
+            console.log(myNumber);
+
+            myNumber = myNumber - 10;
+            
+            console.log(myNumber);
+
+</script>
